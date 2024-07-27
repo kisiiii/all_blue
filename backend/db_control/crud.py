0c3097fb0b4afb5d09be1e77cbde953c9cfbc35c
kishi_masahiro <ksmshr1988@gmail.com>
2024-07-27 22:06:02 +0900

    crud.py 不要コード削除

--- backend/db_control/crud.py
+++ backend/db_control/crud.py
@@ -32,34 +32,6 @@ def myinsert(mymodel, values):
     session.close()
     return "inserted"
 
-"""
-def myselect(mymodel, user_id):
-    # session構築
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    query = session.query(mymodel).filter(mymodel.customer_id == customer_id)
-    try:
-        # トランザクションを開始
-        with session.begin():
-            result = query.all()
-        # 結果をオブジェクトから辞書に変換し、リストに追加
-        result_dict_list = []
-        for customer_info in result:
-            result_dict_list.append({
-                "customer_id": customer_info.customer_id,
-                "customer_name": customer_info.customer_name,
-                "age": customer_info.age,
-                "gender": customer_info.gender
-            })
-        # リストをJSONに変換
-        result_json = json.dumps(result_dict_list, ensure_ascii=False)
-    except sqlalchemy.exc.IntegrityError:
-        print("一意制約違反により、挿入に失敗しました")
-
-    # セッションを閉じる
-    session.close()
-    return result_json
-"""
 
 def myselect(mymodel, user_id):
     Session = sessionmaker(bind=engine)
